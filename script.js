676569d9bd4c84644d373c2b290b39e102c91736
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-21 14:23:06 +0000

    Edit addHoverColor(),create clearPixel(), add event listener to the Clear button

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const slider = document.getElementById('mySlider');
 const currentSize = document.getElementById('currentSize');
 const grid = document.getElementById('grid');
-
-
+const clearBtn = document.getElementById('clear');
 
 makeGrid(slider.value);
 
+slider.oninput = function () {
+    currentSize.textContent = `${slider.value} x ${slider.value}`;
+    deleteGrid();
+    makeGrid(slider.value);
+}
+
 function addHoverColor() {
     const squares = document.querySelectorAll('.pixel');
-}
+    squares.forEach(function (element) {
+        element.addEventListener("mouseenter", function () {
+            element.style.backgroundColor = "black";
+        });
+    });
+};
 
 function makeGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -18,19 +28,24 @@ function makeGrid(size) {
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
             row.appendChild(pixel);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+    addHoverColor();
 }
 
-}
 
 function deleteGrid() {
     grid.textContent = '';
 }
 
-slider.oninput = function() {
-    currentSize.textContent = `${slider.value} x ${slider.value}`;
-    deleteGrid();
-    makeGrid(slider.value);
+function clearPixel() {
+    const squares = document.querySelectorAll('.pixel');
+    squares.forEach(function (element) {
+        element.style = "null";
+    })
 }
 
+clearBtn.addEventListener('click', function () {
+    clearPixel();
+})
